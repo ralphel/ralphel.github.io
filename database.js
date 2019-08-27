56e21a6ce9ff37001561060f51ca3448e9c450df
ralphel <elijahralph@Elijahs-MBP.fios-router.home>
2019-08-27 19:39:40 -0400

    added sites

--- database.js
+++ database.js
@@ -1,4 +1,4 @@
-var site = Array("www.wodc.org", "song2", "song3", "song4", "song5", "song6");
+var site = Array("http://iroquois.tripod.com/ICOC/home.html", "https://www.campmenorah.com/", "http://test.woodwind.org/clarinet/BBoard/read.html?f=1&i=21968&t=21603", "http://www.moelinks.com/", "https://www.ibiblio.org/elvis/oldeps.html", "https://www.ibiblio.org/elvis/elvlinks.html");
 
 function randomSite() {
   var randomSite = site[Math.floor(Math.random() * site.length)];
